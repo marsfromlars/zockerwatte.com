3032ee304c797cde2220c23709a5e640e5dd820a
larsfrommars <larsfrommars@gmail.com>
2017-05-25 21:10:18 +0200

    simple sprite

---
+++ lamosoft/examples/sprite.js
@@ -0,0 +1,53 @@
+function Sprite( config ) {
+    this.el = config.el;
+    this.rows = config.rows || 8;
+    this.cols = config.cols || 8;
+    this.pixelWidth = config.pixelWidth || 10;
+    this.pixelHeight = config.pixelHeight || 10;
+    this.pixels = [];
+    this.ctx = this.el.getContext( '2d' );
+}
+
+Sprite.prototype.repaint = function() {
+    for( var y = 0; y < this.rows; y++ ) {
+        for( var x = 0; x < this.rows; x++ ) {
+            let index = this.getIndex( x, y );
+            let color = this.pixels[ index ];
+            if( color ) {
+                this.paint( x, y, color );
+            }
+            else {
+                this.clearPixel( x, y, color );
+            }
+        }
+    }
+}
+
+Sprite.prototype.getIndex = function( x, y, color ) {
+    return y * this.cols + x;
+}
+
+Sprite.prototype.set = function( x, y, color ) {
+    this.pixels[ this.getIndex( x, y ) ] = color;
+}
+
+Sprite.prototype.get = function( x, y ) {
+    return this.pixels[ this.getIndex( x, y ) ];
+}
+
+Sprite.prototype.paint = function( x, y, color ) {
+    this.ctx.fillStyle = color;
+    this.ctx.fillRect( this.pixelWidth * x, this.pixelHeight * y, this.pixelWidth, this.pixelHeight );
+}
+
+Sprite.prototype.clearPixel = function( x, y, color ) {
+    this.ctx.clearRect( this.pixelWidth * x, this.pixelHeight * y, this.pixelWidth, this.pixelHeight );
+}
+
+Sprite.prototype.clear = function() {
+    this.pixels = [];
+    this.repaint();
+}
+
+
+
