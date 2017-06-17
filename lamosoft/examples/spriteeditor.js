7df4e0754d2c2b8f1ff1b3899d9192a67c963226
larsfrommars <larsfrommars@gmail.com>
2017-06-17 20:27:05 +0200

    add persistence to sprite editor

--- lamosoft/examples/spriteeditor.js
+++ lamosoft/examples/spriteeditor.js
@@ -4,12 +4,17 @@
  * @param {*} config 
  */
 function SpriteEditor( config ) {
+
     this.sprite = config.sprite;
     this.el = this.sprite.el;
     this.color = 'red';
     this.pendown = false;
+    this.persistenceId = config.persistenceId;
 
     var me = this;
+
+    me.load();
+    
     this.el.addEventListener( 'click', function( event ) {
         let x = me.sprite.x2col( event.offsetX );
         let y = me.sprite.y2row( event.offsetY );
@@ -28,15 +33,50 @@ function SpriteEditor( config ) {
             me.doclick( x, y );
         }
     });
+
 }
 
 SpriteEditor.prototype.doclick = function( x, y ) {
     this.sprite.set( x, y, this.color );
     this.sprite.repaint();
+    this.save();
 }
 
 SpriteEditor.prototype.shift = function( x, y ) {
     this.sprite.shift( x, y );
     this.sprite.repaint();
+    this.save();
+}
+
+SpriteEditor.prototype.save = function() {
+    let me = this;
+    if( me.persistenceId ) {
+        let data = {
+            color: this.color,
+            pixels: this.sprite.pixels
+        };
+        let serialized = JSON.stringify( data );
+        window.localStorage.setItem( me.persistenceId, serialized );
+    }
+}
+
+SpriteEditor.prototype.load = function() {
+    let me = this;
+    if( me.persistenceId ) {
+        try {
+            let stored = window.localStorage.getItem( me.persistenceId );
+            if( stored ) {
+                let loaded = JSON.parse( stored );
+                if( loaded ) {
+                    this.color = loaded.color ? loaded.color : 'red';
+                    this.sprite.pixels = loaded.pixels ? loaded.pixels : this.sprite.pixels;
+                    this.sprite.repaint();
+                }
+            }
+        }
+        catch( e ) {
+            console.log( e );
+        }
+    }    
 }
 
