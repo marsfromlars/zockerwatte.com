a1aa67d4b02aee3817a26a4b3ac48b3916179142
Lars Löffler <lars.loeffler@infodesire.com>
2019-07-05 16:21:29 +0200

    add node and javascript/strings

---
+++ lamosoft/learn/node/httpserver.js
@@ -0,0 +1,15 @@
+var http = require('http');
+var url = require('url');
+
+http.createServer(function (req, res) {
+    let url = url.parse( req.url );
+    if( req.method == 'GET' ) {
+            
+    }
+    console.log(req.method + ' ' + req.url);
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World!');
+}).listen(3000);
+
+console.log( 'Http server running on port 3000.' );
+
