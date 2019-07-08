a7d83c6940693a3630d0c152c3f0f006a2c9606c
Lars Löffler <lars.loeffler@infodesire.com>
2019-07-08 21:05:01 +0200

    w.i.p.

--- lamosoft/learn/node/httpserver.js
+++ lamosoft/learn/node/httpserver.js
@@ -3,9 +3,9 @@ var url = require('url');
 
 http.createServer(function (req, res) {
     let url = url.parse( req.url );
-    if( req.method == 'GET' ) {
-            
-    }
+    // if( req.method == 'GET' ) {
+    //     if( url.pathname.
+    // }
     console.log(req.method + ' ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World!');
